2df1cac17b88fc8143950fc851543dd0a72b29c0
w83ll43 <sherillwilkins258@gmail.com>
2024-02-22 21:11:28 +0800

    初始化项目结构

---
+++ api-alliance-web/src/main/java/com/w83ll43/MainApplication.java
@@ -0,0 +1,12 @@
+package com.w83ll43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
